130553d0480cc638e57c70295a54b9f8e431627a
thek0der <horjarazvan@gmail.com>
2023-11-08 22:58:05 +0200

    Updated variable names

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main() {
     int th;
     int **board, **guess;
     int *nums;
-    int opt, rand, gameOn = 1, randuriCorecte = 0;
+    int opt, row, gameOn = 1, correctRows = 0;
 
     time_t start_time, end_time;
     double elapsed_time;
@@ -58,6 +58,9 @@ int main() {
 
     time(&start_time);
 
+    for(int i = 0; i < th; i++)
+        if(nums[i] == 0) correctRows++;
+
     do {
         clear_screen();
         print_screen(guess, nums, th);
@@ -76,29 +79,34 @@ int main() {
 
         case 1: {
             printf("\nIntroduceti indexul randului pe care vreti sa il completati (0, %d): ", th-1);
-            scanf("%d", &rand);
+            scanf("%d", &row);
 
-            if(rand < 0 || rand > th - 1) {
-                printf("Rand invalid! Randul trebuie sa fie intre 0 si %d\n", th-1);
+            if(row < 0 || row > th - 1) {
+                printf("Rand invalid! Indexul randului trebuie sa fie intre 0 si %d\n", th-1);
+                break;
+            }
+
+            if(check_row(guess, board, th, row) || nums[row] == 0) {
+                printf("Randul este deja completat corect!\n");
                 break;
             }
 
             printf("\nIntroduceti valorile randului (cu spatiu intre ele): ");
 
             for(int j = 0; j < th; j++) {
-                scanf("%d", &guess[rand][j]);
+                scanf("%d", &guess[row][j]);
             }
 
             if(!check_binarity(guess, th)) {
-                printf("\nNu ati introdus o reprezentare in binar pentru randul %d!\n", rand);
+                printf("\nNu ati introdus o reprezentare in binar pentru randul %d!\n", row);
                 break;
             }
             
-            if(!check_ans(guess, board, th, rand)) {
+            if(!check_row(guess, board, th, row)) {
                 printf("Valoarea introdusa este incorecta!\n");
-            } else randuriCorecte++;
+            } else correctRows++;
             
-            if(randuriCorecte == th) {
+            if(correctRows == th) {
                 time(&end_time);
                 gameOn = 0;
             }
@@ -122,6 +130,7 @@ int main() {
 
     free(board);
     free(nums);
+    free(guess);
 
     return 0;
 }
